3cd1710f3bcf3150716a30c4884aeeea11449a8e
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-02-09 11:46:14 +0100

    Add files via upload

--- pages/buitenkozijnen.py
+++ pages/buitenkozijnen.py
@@ -6,17 +6,22 @@
 
 import streamlit as st
 import pandas as pd
+from menu import menu_with_redirect
 
 
 # In[ ]:
+
+
+menu_with_redirect()
 
 
 st.title("Projecten Eigen Haard")
 st.header("Buitenkozijnen, -ramen, -deuren en -puien")
 st.markdown("**wat is de range van producten die in een productgroep geïmplementeerd kunnen worden?**")
 
-if option == 'Nieuwbouw woningen':
-    value = st.slider('vul max en min te implementeren producten in',0, 100, (2, 10))
-    besparen = st.number_input("vul hoeveel producten er al geimplementeerd zijn in", min_value=0)
+
+
+value = st.slider('vul max en min te implementeren producten in',0, 100, (2, 10))
+besparen = st.number_input("vul hoeveel producten er al geimplementeerd zijn in", min_value=0)
 
 
