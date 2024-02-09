fe0731fc77372a27b6053ae40df8f8d8c0c2f521
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-02-09 10:46:10 +0100

    Add files via upload

---
+++ pages/buitenkozijnen.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import pandas as pd
+
+
+# In[ ]:
+
+
+st.title("Projecten Eigen Haard")
+st.header("Buitenkozijnen, -ramen, -deuren en -puien")
+st.markdown("**wat is de range van producten die in een productgroep geïmplementeerd kunnen worden?**")
+
+
+value = st.slider('vul max en min te implementeren producten in',0, 100, (2, 10))
+besparen = st.number_input("vul hoeveel producten er al geimplementeerd zijn in", min_value=0)
+
+
