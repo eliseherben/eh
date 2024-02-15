9036af1a7c6203ca10b39b03099e1038153bac69
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-02-15 13:08:45 +0100

    Add files via upload

--- pages/optimalisatie.py
+++ pages/optimalisatie.py
@@ -35,7 +35,23 @@ def f3(x, maximize=True):
     return result if maximize else -result
 
 def f4(x, maximize=True):
+    result = 8*x[0] + 2.5*x[1] + 8*x[2] + 5*x[3] + 6*x[4] + 3*x[5] + 8*x[6] + 8*x[7] + 6*x[8] + 7*x[9] + 7*x[10] + 7.5*x[11] + 8*x[12] + 6*x[13] + 6*x[14] + 4*x[15] + 7*x[16] + 7*x[17] + 7*x[18] + 4*x[19] + 4*x[20] + 7.5*x[21] + 6*x[22] + 5*x[23] + 3*x[24]
+    return result if maximize else -result
+
+def f5(x, maximize=True):
     result = 9.5*x[0] + 10*x[1] + 4*x[2] + 8*x[3] + 6*x[4] + 7*x[5] + 6*x[6] + 8*x[7] + 6.5*x[8] + 4*x[9] + x[10] + 5*x[11] + 5*x[12] + 4*x[13] + 8*x[14] + 6*x[15] + 6*x[16] + 6*x[17] + 8*x[18] + 3*x[19] + 4*x[20] + 4*x[21] + 2*x[22] + 4*x[23] + 6*x[24]
+    return result if maximize else -result
+
+def f6(x, maximize=True):
+    result = 7.5*x[0] + 7.5*x[1] + 8*x[2] + 7.5*x[3] + 6*x[4] + 7.5*x[5] + 8*x[6] + 8*x[7] + 6*x[8] + 7*x[9] + 4*x[10] + 2*x[11] + 5*x[12] + 2.5*x[13] + 6*x[14] + 3*x[15] + 8*x[16] + 8*x[17] + 6*x[18] + 3*x[19] + 4*x[20] + 4*x[21] + 7*x[22] + 5*x[23] + 6*x[24]
+    return result if maximize else -result
+
+def f7(x, maximize=True):
+    result = 4*x[0] + x[1] + 4*x[2] + 2*x[3] + 6*x[4] + 3*x[5] + x[6] + 4*x[7] + 4*x[8] + 4*x[9] + 2*x[10] + 4*x[11] + 6*x[12] + 2.5*x[13] + 3*x[14] + 2.5*x[15] + 4*x[16] + 4*x[17] + 4*x[18] + x[19] + 3*x[20] + 4*x[21] + 4*x[22] + 2.5*x[23] + 4*x[24]
+    return result if maximize else -result
+
+def f8(x, maximize=True):
+    result = 4*x[0] + x[1] + 9*x[2] + 8*x[3] + 5*x[4] + 6*x[5] + 9*x[6] + 9*x[7] + 6*x[8] + 4*x[9] + 2*x[10] + x[11] + x[12] + 5*x[13] + 2*x[14] + 2*x[15] + 8*x[16] + 6*x[17] + 7*x[18] + 2.5*x[19] + 4*x[20] + 2*x[21] + 4*x[22] + 7*x[23] + 3*x[24]
     return result if maximize else -result
     
 # Check if solution x dominates solution y
@@ -99,7 +115,7 @@ def pareto_simulated_annealing(objective_functions, initial_solution, max_iterat
 
 # Example usage
 initial_solution = [random.randint(0, 20) for _ in range(25)]  # Initial solution with random integers
-objective_functions = [f1, f2, f3, f4]
+objective_functions = [f1, f2, f3, f4, f5, f6]
 pareto_front = pareto_simulated_annealing(objective_functions, initial_solution, max_iterations=1000, max_temperature=100, min_temperature=0.01, cooling_rate=0.95)
 
 st.markdown("## Pareto Front:")
@@ -112,8 +128,9 @@ for solution in pareto_front:
     st.markdown(f"- aanschafkosten: {f1(solution)} ")
     st.markdown(f"- onderhoudskosten: {f2(solution)}")
     st.markdown(f"- mate van losmaakbaarheid: {f3(solution)}")
-    st.markdown(f"- woonbeleving: {f4(solution)}")
-
+    st.markdown(f"- toepassingsmogelijkheden: {f4(solution)}")
+    st.markdown(f"- woonbeleving: {f5(solution)}")
+    st.markdown(f"- milieubelasting: {f6(solution)}")
 
 
 # In[ ]:
