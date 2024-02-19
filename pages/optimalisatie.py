08e9a9e729229c9d7c46d5322606f792581a592d
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-02-19 13:08:32 +0100

    Add files via upload

--- pages/optimalisatie.py
+++ pages/optimalisatie.py
@@ -15,6 +15,8 @@ menu_with_redirect()
 st.title("Optimalisatie")
 
 
+# ### Simulated annealing
+
 # In[ ]:
 
 
@@ -149,5 +151,138 @@ for solution in pareto_front:
 # In[ ]:
 
 
+# def startoplossingen()
+#     oplossingen = []
+#     while len(oplossingen) < 10:
+#         oplossing = [random.randint(0, 20) for _ in range(25)]
+#         oplossingen.append(oplossing)
 
+
+# ### Genetic algorithm
+
+# In[ ]:
+
+
+def f1(x):
+    result = 6*x[0] + 9*x[1] + 2*x[2] + 7*x[3] + 4*x[4] + 4*x[5] + 6*x[6] + x[7] + 4*x[8] + 7*x[9] + 9*x[10] + 10*x[11] + 4*x[12] + 2*x[13] + 6*x[14] + 6*x[15] + 2*x[16] + x[17] + 5*x[18] + 4*x[19] + 4*x[20] + 10*x[21] + 2*x[22] + 2.5*x[23] + 9*x[24]
+    return result 
+
+def f2(x):
+    result = 4.5*x[0] + 2*x[1] + 6*x[2] + 4*x[3] + 4*x[4] + 5*x[5] + 10*x[6] + 3*x[7] + 6*x[8] + x[9] + x[10] + x[11] + 4*x[12] + 9*x[13] + 7.5*x[14] + 4*x[15] + 2*x[16] + x[17] + 3*x[18] + 4*x[19] + 3*x[20] + 2.5*x[21] + 4*x[22] + 6*x[23] + 5*x[24]
+    return result 
+
+def f3(x):
+    result = 9.5*x[0] + 3*x[1] + 9*x[2] + 4.5*x[3] + x[4] + 3*x[5] + 9*x[6] + 9*x[7] + 4*x[8] + 6*x[9] + 8*x[10] + 2*x[11] + 9*x[12] + 4*x[13] + 6*x[14] + 6*x[15] + 4*x[16] + 6*x[17] + 7*x[18] + 4*x[19] + 6*x[20] + 6*x[21] + 8*x[22] + 7*x[23] + 4*x[24]
+    return result 
+
+def f4(x):
+    result = 8*x[0] + 2.5*x[1] + 8*x[2] + 5*x[3] + 6*x[4] + 3*x[5] + 8*x[6] + 8*x[7] + 6*x[8] + 7*x[9] + 7*x[10] + 7.5*x[11] + 8*x[12] + 6*x[13] + 6*x[14] + 4*x[15] + 7*x[16] + 7*x[17] + 7*x[18] + 4*x[19] + 4*x[20] + 7.5*x[21] + 6*x[22] + 5*x[23] + 3*x[24]
+    return result 
+
+def f5(x):
+    result = 9.5*x[0] + 10*x[1] + 4*x[2] + 8*x[3] + 6*x[4] + 7*x[5] + 6*x[6] + 8*x[7] + 6.5*x[8] + 4*x[9] + x[10] + 5*x[11] + 5*x[12] + 4*x[13] + 8*x[14] + 6*x[15] + 6*x[16] + 6*x[17] + 8*x[18] + 3*x[19] + 4*x[20] + 4*x[21] + 2*x[22] + 4*x[23] + 6*x[24]
+    return result 
+
+def f6(x):
+    result = 7.5*x[0] + 7.5*x[1] + 8*x[2] + 7.5*x[3] + 6*x[4] + 7.5*x[5] + 8*x[6] + 8*x[7] + 6*x[8] + 7*x[9] + 4*x[10] + 2*x[11] + 5*x[12] + 2.5*x[13] + 6*x[14] + 3*x[15] + 8*x[16] + 8*x[17] + 6*x[18] + 3*x[19] + 4*x[20] + 4*x[21] + 7*x[22] + 5*x[23] + 6*x[24]
+    return result 
+
+def f7(x):
+    result = 4*x[0] + x[1] + 4*x[2] + 2*x[3] + 6*x[4] + 3*x[5] + x[6] + 4*x[7] + 4*x[8] + 4*x[9] + 2*x[10] + 4*x[11] + 6*x[12] + 2.5*x[13] + 3*x[14] + 2.5*x[15] + 4*x[16] + 4*x[17] + 4*x[18] + x[19] + 3*x[20] + 4*x[21] + 4*x[22] + 2.5*x[23] + 4*x[24]
+    return result 
+
+def f8(x):
+    result = 4*x[0] + x[1] + 9*x[2] + 8*x[3] + 5*x[4] + 6*x[5] + 9*x[6] + 9*x[7] + 6*x[8] + 4*x[9] + 2*x[10] + x[11] + x[12] + 5*x[13] + 2*x[14] + 2*x[15] + 8*x[16] + 6*x[17] + 7*x[18] + 2.5*x[19] + 4*x[20] + 2*x[21] + 4*x[22] + 7*x[23] + 3*x[24]
+    return result 
+
+def uitkomsten(oplossing):
+    return -f1(oplossing) - f2(oplossing) + f3(oplossing) + f4(oplossing) + f5(oplossing) - f6(oplossing) + f7(oplossing) + f8(oplossing)
+
+def startoplossingen():
+    oplossingen = []
+    while len(oplossingen) < 10:
+        oplossing = [random.randint(0, 20) for _ in range(25)]
+        oplossingen.append((oplossing, uitkomsten(oplossing)))
+    oplossingen.sort(key=lambda uitkomsten: uitkomsten[1], reverse=True)
+    return oplossingen
+
+
+# In[ ]:
+
+
+def ouders_maken(populatie):
+    ouders = []
+    beste = [i for i in populatie[0:5]]
+    slechtste = [i for i in populatie[5:]] 
+    i = 0
+    while i < 4:    
+        ouders.append(random.choice(beste))
+        i = i + 1
+
+    j = 0
+    while j < 2:
+        ouders.append(random.choice(slechtste))
+        j = j + 1
+    return ouders
+
+
+# In[ ]:
+
+
+def kinderen_maken(ouders):
+    kinderen = []
+    while len(ouders) != 0:
+        parent1, parent2 = random.sample(ouders, 2)
+        ouders.remove(parent1)
+        ouders.remove(parent2)
+        
+        kind1 = random.choice([parent1, parent2])
+        kind2 = random.choice([parent1, parent2])
+        kinderen.append(kind1)
+        kinderen.append(kind2)
+    return kinderen
+
+
+# In[ ]:
+
+
+populatie = startoplossingen()
+iteraties = 0
+while iteraties < 5:
+    populatie = [tuple(i[0]) for i in populatie]
+    ouders = ouders_maken(populatie)
+    populatie = [i for i in populatie[0:4]]
+    kinderen = kinderen_maken(ouders)
+    for a in kinderen:
+        populatie.append(tuple(a))
+    for x in range(len(populatie)):
+        populatie[x] = (list(populatie[x]), uitkomsten(populatie[x]))
+    populatie.sort(key=lambda uitkomsten: uitkomsten[1], reverse=True)
+    populatie = populatie
+    iteraties = iteraties + 1
+
+
+# In[ ]:
+
+
+st.markdown("### Genetic algorithm:")
+i = 0
+for solution in populatie:
+    i = i + 1
+    j = 0
+    st.markdown(f"#### Oplossing {i}")
+    st.markdown(f"totale score: {uitkomsten(solution)}")
+    for product in productgroepen:
+        st.markdown(f"Aantal producten in de productgroep {product} {solution[j]}")
+        j = j + 1
+    st.markdown(solution)
+    st.markdown("#### Objective Values:")
+    st.markdown(f"- aanschafkosten: {f1(solution)} ")
+    st.markdown(f"- onderhoudskosten: {f2(solution)}")
+    st.markdown(f"- mate van losmaakbaarheid: {f3(solution)}")
+    st.markdown(f"- toepassingsmogelijkheden: {f4(solution)}")
+    st.markdown(f"- woonbeleving: {f5(solution)}")
+    st.markdown(f"- milieubelasting: {f6(solution)}")
+    st.markdown(f"- flexibiliteit tbv toekomstbestendigheid en innovatie: {f7(solution)}")
+    st.markdown(f"- mate van standaardisering: {f8(solution)}")
 
