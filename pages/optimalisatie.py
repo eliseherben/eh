c8d5bac2e24c020b56ec123b72c9f0f6bd644a08
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-02-26 09:07:14 +0100

    Add files via upload

--- pages/optimalisatie.py
+++ pages/optimalisatie.py
@@ -368,7 +368,7 @@ while iteraties < 5:
 st.markdown("### Genetic algorithm:")
 i = 0
 populatie = [tuple(i[0]) for i in populatie]
-wegingen = [st.session_state.aanschafprijs, st.session_state.onderhoudskosten, st.session_state.losmaakbaarheid, 
+wegingen = [st.session_state.aanschafprijs, st.session_state.onderhoudsprijs, st.session_state.losmaakbaarheid, 
             st.session_state.toepassingsmogelijkheden, st.session_state.woonbeleving, st.session_state.milieubelasting, 
            st.session_state.flexibiliteit, st.session_state.standaardisering]
 st.markdown(wegingen)
