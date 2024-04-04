6cdd5c449f0af6ee4917101273af372f191b2f03
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-04 11:03:03 +0200

    Add files via upload

--- pages/simplex.py
+++ pages/simplex.py
@@ -529,7 +529,7 @@ with tab3:
         woonbeleving2 = sum([df['Waarde'][i] * impact_woonbeleving[i] for i in range(25)])
         kwaliteit2 = sum([df['Waarde'][i] * impact_kwaliteit[i] for i in range(25)])
         onderhoud2 = sum([df['Waarde'][i] * impact_onderhoud[i] for i in range(25)])
-        st.markdown(f"score duurzaamheid: {duurzaam2/sum(impact_duurzaam)}")
+        st.markdown(f"score duurzaamheid: {duurzaam2/sum(impact_duurzaamheid)}")
         st.markdown(f"score prijs: {prijs2/sum(impact_prijs)}")
         st.markdown(f"score woonbeleving: {woonbeleving2/sum(impact_woonbeleving)}")
         st.markdown(f"score kwaliteit: {kwaliteit2/sum(impact_kwaliteit)}")
