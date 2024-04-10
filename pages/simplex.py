7abfe379271cfbfb7ea5837c0f0f8d9abfbf9038
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-04-10 09:00:48 +0200

    Add files via upload

--- pages/simplex.py
+++ pages/simplex.py
@@ -53,7 +53,7 @@ with tab1:
     st.markdown("**Projectbestand**")
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
-        dataframe = pd.read_csv(uploaded_file)
+        dataframe = pd.read_csv(uploaded_file, sep = ;)
         st.write(dataframe)
     
     st.markdown("**Budget**")
